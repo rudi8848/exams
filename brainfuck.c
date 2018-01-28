bb7763b6117909e908f142e6a55b553f47fd4031
Ganna VYNOGRADOVA <gvynogra@e3r8p3.unit.ua>
2018-01-28 16:53:55 +0200

    some changes

--- brainfuck.c
+++ brainfuck.c
@@ -1,54 +1,32 @@
-//#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-void	ft_putchar(char c)
+void    ft_brainfuck(char *str)
 {
-	write(1, &c, 1);
+    int        i;
+    char    *ptr;
+
+    i = 0;
+    ptr = (char*)malloc(sizeof(char*) * 2048);
+    while (str[i] != 0)
+    {
+        *ptr += str[i] == '+' ? 1 : 0;
+        *ptr -= str[i] == '-' ? 1 : 0;
+        ptr += str[i] == '>' ? 1 : 0;
+        ptr -= str[i] == '<' ? 1 : 0;
+        if (str[i] == '[' && *ptr == 0)
+            while (str[i] != ']')
+                i++;
+        if (str[i] == ']' && *ptr != 0)
+            while (str[i] != '[')
+                i--;
+        if (str[i] == '.')
+            write(1, ptr, 1);
+        i++;
+    }
 }
 
-void	interpret(char *msg)
+int        main()
 {
-	char arr[30000] = {0};
-	char *p = arr;
-	int i = 0;
-	int loop = 0;
-	int j = 0;
-
-	while (i < strlen(msg))
-	{
-		if (msg[i] == '>')
-			p++;
-		else if (msg[i] == '<')
-			p--;
-		else if (msg[i] == '+')
-			(*p)++;
-		else if (msg[i] == '-')
-			(*p)--;
-		else if (msg[i] == '.')
-			ft_putchar(*p);
-		else if (msg[i] == '[')
-				loop = i-1;
-		else if (msg[i] == ']')
-		{
-			while (msg[loop])
-			{
-				msg[loop]--;
-				i = loop;
-				continue;
-			}
-		}
-		
-		i++;
-	}
-}
-
-int		main(int argc, char *argv[])
-{
-	if (argc == 2)
-		interpret(argv[1]);
-	else
-		printf("usage: %s <message>\n", argv[0]);
-	return 0;
+    ft_brainfuck("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.");  
 }
